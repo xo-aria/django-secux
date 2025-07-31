bdcd227d31c15a5ffb58e92912e7838682801874
XO Aria <104579901+xo-aria@users.noreply.github.com>
2025-07-31 12:51:04 +0330

    Update apps.py

--- django_secux/apps.py
+++ django_secux/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 class DjangoSecuxConfig(AppConfig):
     name = 'django_secux'
     verbose_name = 'Django Secux'
+
+    def ready(self):
+        from . import signals
