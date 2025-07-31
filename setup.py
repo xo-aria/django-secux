931b1b1fed3c123a83b53e1f8d02f2e09c94c213
XO Aria <104579901+xo-aria@users.noreply.github.com>
2025-07-31 22:06:43 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-secux',
-    version='4.6.0',
+    version='5.6.0',
     description='A lightweight Django security package with rate limiting decorator',
     author='XO Aria',
     author_email='hf18950@gmail.com',
