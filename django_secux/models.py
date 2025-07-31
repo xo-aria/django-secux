b350b72ddd4fdcbd200d7412aa4be59be3b6642f
XO Aria <104579901+xo-aria@users.noreply.github.com>
2025-07-31 14:31:35 +0330

    Update models.py

--- django_secux/models.py
+++ django_secux/models.py
@@ -39,22 +39,32 @@ class OptimizedImageFile(ImageFieldFile):
         try:
             quality = getattr(self.field, 'quality', 70)
             file_name = getattr(self.field, 'custom_name', name)
+            size = getattr(self.field, 'resize_to', None)
+
             image = Image.open(content)
+
             if image.mode in ("RGBA", "P"):
                 image = image.convert("RGB")
+
+            if size and isinstance(size, tuple) and len(size) == 2:
+                image = image.resize(size, Image.ANTIALIAS)
+
             buffer = BytesIO()
             image.save(buffer, format='JPEG', quality=quality, optimize=True)
             buffer.seek(0)
             content = ContentFile(buffer.read(), name=file_name)
-        except Exception:
-            pass
+
+        except Exception as e:
+            print(f"[OptimizeImageField Error] {e}")
+
         super().save(name, content, save)
 
 
 class OptimizeImageField(models.ImageField):
     attr_class = OptimizedImageFile
 
-    def __init__(self, *args, quality=70, name=None, **kwargs):
+    def __init__(self, *args, quality=70, name=None, size=None, **kwargs):
         self.quality = quality
         self.custom_name = name
+        self.resize_to = size
         super().__init__(*args, **kwargs)
